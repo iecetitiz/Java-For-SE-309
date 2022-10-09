2fa0969d78aa7bbcddcb06e23399d22f905dc759
iecetitiz <i.ecetitiz@gmail.com>
2022-10-09 09:05:53 +0300

    Switch case statements are added

--- java_se309/src/Main.java
+++ java_se309/src/Main.java
@@ -1,7 +1,17 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int a = 5;
+
         System.out.print("Please enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        switch (number) {
+            case 1: System.out.print("Bir"); break;
+            case 2: System.out.print("Iki"); break;
+            case 3: System.out.print("Uc"); break;
+            default: System.out.print("Default");
+        }
+
+
     }
 }
